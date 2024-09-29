83e51eea70057293427592c53276bd6e8583d98c
unknown <fahrurrozzi18@gmail.com>
2024-09-29 17:15:59 +0700

    update navbar and hero

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,25 @@
+import "./app.scss";
+import Hero from "./components/hero/Hero";
+import Navbar from "./components/navbar/Navbar";
+import Test from "./Test";
+
 const App = () => {
-  return <div>Hello World</div>;
+  return (
+    <div>
+      <section id="Homepage">
+        <Navbar />
+        <Hero />
+      </section>
+      <section id="Services">Parallax</section>
+      <section>Services</section>
+      <section id="Portofolio">Parallax</section>
+      <section>Portofolio1</section>
+      <section>Portofolio2</section>
+      <section>Portofolio3</section>
+      <section id="Contact">Contact</section>
+      {/* <Test /> */}
+    </div>
+  );
 };
 
 export default App;
